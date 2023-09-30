0bcc641e4965278437c1d79996709c06ed265234
Daniel <danieldelsermartin@gmail.com>
2023-09-30 11:47:22 +0200

    Siguiente commit

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/cursogo/variables"
+	"runtime"
+	// "github.com/cursogo/variables"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1588)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	// estado, texto := variables.ConviertoaTexto(1588)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("No es windows")
+	} else {
+		fmt.Println("Es windows", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
